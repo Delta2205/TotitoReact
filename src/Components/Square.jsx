de10fd32e9b9f50529a5e9fad35ceffbff107eca
Delta2205 <pvasquez2206@gmail.com>
2024-08-23 13:25:27 -0600

    se Agrego un folder que contienen los distintos componentes

---
+++ src/Components/Square.jsx
@@ -0,0 +1,12 @@
+export const Square = ({children,updateBoard,index, isSelected })=>{
+  const className = `square ${isSelected ? 'is-selected':''}`
+  const handlClick = () =>{
+    updateBoard(index)
+  }
+
+  return(
+    <div className={className} onClick={handlClick}>
+      {children}
+    </div>
+  )
+}
