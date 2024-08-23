f9eac7794332ad5770350fb164ba9e4d60911cf9
Delta2205 <pvasquez2206@gmail.com>
2024-08-23 01:00:17 -0600

    Render logica del tablero

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import "./App.css";
+import { useState } from "react";
 const TURNS = {
   X: "x",
   O: "o",
 };
 
-const Square = ({children,updateBoard,index })=>{
+const Square = ({children,updateBoard,index, isSelected })=>{
+  const className = `square ${isSelected ? 'is-selected':''}`
+  const handlClick = () =>{
+    updateBoard(index)
+  }
+
   return(
-    <div className="square">
+    <div className={className} onClick={handlClick}>
       {children}
     </div>
   )
 }
-const board = Array(9).fill(null);
+
 function App() {
+  const [board,setBoard] = useState(Array(9).fill(null))
+
+  const [turn,setTurn] = useState(TURNS.X)
+
+  const updateBoard = (index) =>{
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+
   return (
     <main className="board">
       <h1>Totito</h1>
@@ -21,11 +39,15 @@ function App() {
           return (
             <div className="cell" key={index}>
               <span className="cell_content">
-                <Square key={index} index={index}>{index}</Square>
+                <Square key={index} index={index} updateBoard={updateBoard} >{board[index]}</Square>
               </span>
             </div>
           );
         })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   );
