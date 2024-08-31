6acd49bd47491bbe4d964ff85d573c454bbaeb29
Delta2205 <pvasquez2206@gmail.com>
2024-08-31 00:49:36 -0600

    format square

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,12 +1,12 @@
-export const Square = ({children,updateBoard,index, isSelected })=>{
-  const className = `square ${isSelected ? 'is-selected':''}`
-  const handlClick = () =>{
-    updateBoard(index)
-  }
+export const Square = ({ children, updateBoard, index, isSelected }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+  const handlClick = () => {
+    updateBoard(index);
+  };
 
-  return(
+  return (
     <div className={className} onClick={handlClick}>
       {children}
     </div>
-  )
-}
+  );
+};
